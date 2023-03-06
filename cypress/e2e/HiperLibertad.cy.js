aa9a9e188d04deae43e934413a29d29f82c27dd1
Hooliper <cesarjulianpereira@gmail.com>
2023-03-06 11:48:04 -0300

    Agregados de seleccion de provincia

--- cypress/e2e/HiperLibertad.cy.js
+++ cypress/e2e/HiperLibertad.cy.js
@@ -3,12 +3,28 @@ describe ('buscar en Hiper Libertad', () =>{
       cy.viewport(1366, 768)
       cy.visit('https://www.hiperlibertad.com.ar/')
     
-      cy.contains('retiro en tienda').click()
-      cy.get('.styles__CloseButton-sc-3xue66-2').click()
+      //cy.contains('retiro en tienda').click()
 
+      //Selecciona Retiro en tienda
+      cy.get('div.styles__Content-sc-1grwrp2-1 > button:nth-child(2)').click()
+      
+      //cy.get('.styles__CloseButton-sc-3xue66-2').click()
+      
+      //Selecciona Provincia
+      cy.get('div#state > div.react-select__control').click()
+      cy.contains('Misiones').click()
+
+      //Selecciona tienda
+      cy.get('div#store > div.react-select__control').click()
+      cy.contains('MISIONES - Hipermercado Posadas').click()
+
+
+
+      /*
       cy.get('input[placeholder="Buscar productos por nombre..."]').type('aceite')
 
       cy.contains('resultados').click()
+      */
 
 
     })
